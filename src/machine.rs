d1a46415ecad7368dc4b2b7897ecefc3c6430a23
Arne Dußin <arne.dussin@live.de>
2017-11-16 22:00:37 +0100

    dirty state

--- src/machine.rs
+++ src/machine.rs
@@ -9,4 +9,33 @@ pub struct Machine {
 	fs: Vec<(PathBuf, SystemTime)>
 }
 
+impl Machine {
+	pub fn new(time: SystemTime) -> Machine {
+		Machine {
+			time: time,
+			fs: Vec::new()
+		}
+	}
+
+	pub fn new_now() -> Machine {
+		Machine {
+			time: SystemTime::now(),
+			fs: Vec::new()
+		}
+	}
+
+	pub fn new_with_fs(time: SystemTime, fs: Vec<(PathBuf, SystemTime)>) -> Machine {
+		Machine {
+			time: time,
+			fs: fs
+		}
+	}
+
+	/// Campare the file system state of this machine to the state of the other
+	/// machine. Returns all the files that are newer on the other Machine.
+	/// XXX: Find a better name for this function.
+	pub fn compare_fs_state(other: &Machine) -> Vec<(PathBuf, SystemTime)> {
+		
+	}
+}
 
